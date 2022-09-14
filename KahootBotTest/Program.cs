21ae6b2b6d3d5166490f67495002398f38b0918e
kentx <aaronrichards4321@gmail.com>
2022-09-14 16:05:35 -0400

    Added automation with selenium

--- KahootBotTest/Program.cs
+++ KahootBotTest/Program.cs
@@ -3,16 +3,50 @@ using System.Security.Principal;
 using HtmlAgilityPack;
 using Katoot;
 using Newtonsoft.Json;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Safari;
 
+//gunna have to use selenium 
+
+//or websocket
 var client = new HttpClient();
 client.BaseAddress = new Uri("https://kahoot.it/rest/kahoots/");
+Console.WriteLine("Enter the username you wish to use");
+string? username = null;
+while (string.IsNullOrEmpty(username))
+{
+    username = Console.ReadLine();
+}
 
 Console.WriteLine("Enter First Letters of Game UUID");
 var id = Console.ReadLine();
+
+Console.WriteLine("Enter the PIN");
+string? pin = null;
+while (string.IsNullOrEmpty(pin))
+{
+    pin = Console.ReadLine();
+}
+
+var opt = new SafariOptions();
+var web = new SafariDriver(opt);
+web.Navigate().GoToUrl("https://kahoot.it?pin=" + pin);
+web.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
+var p = web.FindElement(By.Name("nickname"));
+var b = web.FindElement(By.TagName("button"));
+p.SendKeys(username);
+b.Click();
+Console.ReadKey();
+
+
+Environment.Exit(1);
+
+
 Console.WriteLine("Now Enter Game Title");
 string? game = null;
 
-while (game == null)
+while (string.IsNullOrEmpty(game))
 { 
     game = Console.ReadLine();
 }
